212ca3a8521fb1d93fae57b1079968e8aaf785ba
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-01-29 15:27:59 +0300

    Create Signup Page

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-useless-constructor */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';    
 
-import { FirebaseContext } from '../Firebase';
+import { withFirebase  } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
-        <FirebaseContext.Consumer>
-            {firebase => <SignUpForm firebase={firebase} />}
-        </FirebaseContext.Consumer>
+        <SignUpForm />
     </div>
 );
 
@@ -22,7 +21,7 @@ const INITIAL_STATE = {
     error: null,
   };
 
-class SignUpForm extends Component {
+  class SignUpFormBase extends Component {
     constructor(props) {
       super(props);
 
@@ -37,6 +36,7 @@ class SignUpForm extends Component {
             .doCreateUserWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
                 this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
                 this.setState({ error });
@@ -111,6 +111,11 @@ class SignUpForm extends Component {
       Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
     </p>
   );
+
+  const SignUpForm = compose(
+    withRouter,
+    withFirebase,
+  )(SignUpFormBase);
 
 export default SignUpPage;
 
