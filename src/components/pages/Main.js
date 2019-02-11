2b219b2e0377feb4cad38f64af27a4b15c6677d3
Sam Kasyoki <samkasyoki@yahoo.com>
2019-02-11 08:31:19 +0300

    add games to the website

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 
 export default class Main extends Component {
-  componentDidMount() {
-    setInterval(() => {
-      fetch("https://9a5vg2jktl.execute-api.us-east-1.amazonaws.com/prod")
-        .then(res => res.json())
-        .then(data => {
-          if (data.type === "dashboard") {
-            this.props.history.replace("/");
-          } else if (data.type === "daily-word") {
-            this.props.history.replace("/word");
-          } else if (data.type === "play-time") {
-            this.props.history.replace("/play");
-          } else if (data.type === "story") {
-            this.props.history.replace("/story");
-          }
-        });
-    }, 3000);
-  }
+  //   componentDidMount() {
+  //     setInterval(() => {
+  //       fetch("https://9a5vg2jktl.execute-api.us-east-1.amazonaws.com/prod")
+  //         .then(res => res.json())
+  //         .then(data => {
+  //           if (data.type === "dashboard") {
+  //             this.props.history.replace("/");
+  //           } else if (data.type === "daily-word") {
+  //             this.props.history.replace("/word");
+  //           } else if (data.type === "play-time") {
+  //             this.props.history.replace("/play");
+  //           } else if (data.type === "story") {
+  //             this.props.history.replace("/story");
+  //           }
+  //         });
+  //     }, 3000);
+  //   }
   render() {
     return (
       <div>
