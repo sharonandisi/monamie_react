fa6d86a72dbf2848fdaeea68050e7f8f5959e2b7
maxwaiyaki <maxwel.wairimu@gmail.com>
2019-02-26 12:16:54 +0300

    build done

---
+++ build/precache-manifest.75e415b5a3cf49c68c69fd85eb1d347c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd7c6a9c42e24ab2b38031f9d037664",
+    "url": "/sharonandisi/monamie_react/static/media/monamiehills.fdd7c6a9.jpg"
+  },
+  {
+    "revision": "69a39534790f22669a4b77029ae9bfed",
+    "url": "/sharonandisi/monamie_react/static/media/black-board.69a39534.jpg"
+  },
+  {
+    "revision": "20db72372d62510c83669dd3cb140787",
+    "url": "/sharonandisi/monamie_react/static/media/backgroundmonamie.20db7237.jpg"
+  },
+  {
+    "revision": "f5e66bdd7c2871131525",
+    "url": "/sharonandisi/monamie_react/static/js/runtime~main.f5e66bdd.js"
+  },
+  {
+    "revision": "bb5547a7bb950b88b043",
+    "url": "/sharonandisi/monamie_react/static/js/main.bb5547a7.chunk.js"
+  },
+  {
+    "revision": "16ad155fbab5d8e5dc36",
+    "url": "/sharonandisi/monamie_react/static/js/2.16ad155f.chunk.js"
+  },
+  {
+    "revision": "bb5547a7bb950b88b043",
+    "url": "/sharonandisi/monamie_react/static/css/main.75716205.chunk.css"
+  },
+  {
+    "revision": "cdfcd90408ae98456670df5dbf762c1c",
+    "url": "/sharonandisi/monamie_react/index.html"
+  }
+];
